767182d142dd475f63dd6bdc95d62d6ddb7d12e0
itsfoxdog <aarushverma.ryan@gmail.com>
2023-07-22 22:52:06 +0530

    init

---
+++ components/Hero.tsx
@@ -0,0 +1,51 @@
+import Image from "next/image";
+
+const Hero = () => {
+  return (
+    <div className="flex">
+      <div className="container mx-auto mt-10">
+        <span>T</span>
+        <span>h</span>
+        <span>e</span>
+        <span>-</span>
+        <span>U</span>
+        <span>n</span>
+        <span>y</span>
+        <span>i</span>
+        <span>e</span>
+        <span>l</span>
+        <span>d</span>
+        <span>i</span>
+        <span>n</span>
+        <span>g</span>
+        <span>-</span>
+        <span>P</span>
+        <span>a</span>
+        <span>t</span>
+        <span>t</span>
+        <span>h</span>
+        <span>-</span>
+        <span>O</span>
+        <span>f</span>
+        <span>-</span>
+        <span>S</span>
+        <span>U</span>
+        <span>C</span>
+        <span>C</span>
+        <span>E</span>
+        <span>S</span>
+        <span>-----</span>
+        <span>H</span>
+        <span>a</span>
+        <span>r</span>
+        <span>d</span>
+        <span>w</span>
+        <span>o</span>
+        <span>r</span>
+        <span>k</span>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
