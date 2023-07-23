331bb0a507610be4a93ef01eb656e7410ade2ee5
itsfoxdog <aarushverma.ryan@gmail.com>
2023-07-23 21:01:56 +0530

    init

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,51 +1,31 @@
 "use client";
 
 import Image from "next/image";
+import Typewriter from "typewriter-effect";
+import react from "react";
 
 const Hero = () => {
   return (
-    <div className="">
+    <div>
       <div className="container mx-auto mt-10 font-extrabold text-4xl ml-10 flex justify-center ">
-        <span className="text-black">T</span>
-        <span className="text-black">h</span>
-        <span className="text-black">e</span>
-        <span className="text-black">-</span>
-        <span className="text-black">U</span>
-        <span className="text-black">n</span>
-        <span className="text-black">y</span>
-        <span className="text-black">i</span>
-        <span className="text-black">e</span>
-        <span className="text-black">l</span>
-        <span className="text-black">d</span>
-        <span className="text-black">i</span>
-        <span className="text-black">n</span>
-        <span className="text-black">g</span>
-        <span className="text-black">-</span>
-        <span className="text-black">P</span>
-        <span className="text-black">a</span>
-        <span className="text-black">t</span>
-        <span className="text-black">t</span>
-        <span className="text-black">h</span>
-        <span className="text-black">-</span>
-        <span className="text-black">O</span>
-        <span className="text-black">f</span>
-        <span className="text-black">-</span>
-        <span className="text-black">S</span>
-        <span className="text-black">U</span>
-        <span className="text-black">C</span>
-        <span className="text-black">C</span>
-        <span className="text-black">E</span>
-        <span className="text-black">S</span>
-        <span className="text-black">S</span>
-        <span className="text-black">-----</span>
-        <span className="text-black">H</span>
-        <span className="text-black">a</span>
-        <span className="text-black">r</span>
-        <span className="text-black">d</span>
-        <span className="text-black">w</span>
-        <span className="text-black">o</span>
-        <span className="text-black">r</span>
-        <span className="text-black">k</span>
+        <h1>
+          {
+            <Typewriter
+              onInit={(typewriter) => {
+                for (let i = 2; i > 0; i++) {
+                  typewriter
+                    .typeString("GeeksForGeeks")
+                    .pauseFor(1000)
+                    .deleteAll()
+                    .typeString("Welcomes You")
+                    .pauseFor(1000)
+                    .deleteAll()
+                    .start();
+                }
+              }}
+            />
+          }
+        </h1>
       </div>
     </div>
   );
