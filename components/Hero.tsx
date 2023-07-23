5fd3c5d0ea62c7a1d8913eb262fb3887a9f5f65c
itsfoxdog <aarushverma.ryan@gmail.com>
2023-07-23 22:12:36 +0530

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,7 +7,7 @@ import react from "react";
 const Hero = () => {
   return (
     <div>
-      <div className="container mx-auto mt-10 font-extrabold text-4xl ml-10 flex justify-center sm:text-xl">
+      <div className="container mx-auto mt-10 font-extrabold text-4xl ml-10 flex justify-center ">
         <h1>
           {
             <Typewriter
