9fdaa0260a66b7783243970f950ac69f727e0016
itsfoxdog <aarushverma.ryan@gmail.com>
2023-07-23 22:01:18 +0530

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -13,10 +13,7 @@ const Hero = () => {
             <Typewriter
               onInit={(typewriter) => {
                 typewriter
-                  .typeString("GeeksForGeeks")
-                  .pauseFor(1000)
-                  .deleteAll()
-                  .typeString("Welcomes You")
+                  .typeString("The Unyielding Path to Success: Hard Work")
                   .start();
               }}
             />
