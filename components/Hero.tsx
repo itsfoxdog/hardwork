6d7241a6abb9114de775668a1009f4d06ad2c72c
itsfoxdog <aarushverma.ryan@gmail.com>
2023-07-23 21:53:02 +0530

    init

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,31 +1,25 @@
 "use client";
 
 import Image from "next/image";
-import Typewriter from "typewriter-effect";
+import Typewriter, { TypewriterClass } from "typewriter-effect";
 import react from "react";
 
 const Hero = () => {
+  const animation = (typewriter: TypewriterClass) => {
+    typewriter
+      .typeString("GeeksForGeeks")
+      .pauseFor(1000)
+      .deleteAll()
+      .typeString("Welcomes You")
+      .pauseFor(1000)
+      .deleteAll()
+      .start();
+    animation(typewriter);
+  };
   return (
     <div>
       <div className="container mx-auto mt-10 font-extrabold text-4xl ml-10 flex justify-center ">
-        <h1>
-          {
-            <Typewriter
-              onInit={(typewriter) => {
-                // for (let i = 1; i > 0; i++) {
-                typewriter
-                  .typeString("GeeksForGeeks")
-                  .pauseFor(1000)
-                  .deleteAll()
-                  .typeString("Welcomes You")
-                  .pauseFor(1000)
-                  .deleteAll()
-                  .start();
-                // }
-              }}
-            />
-          }
-        </h1>
+        <h1>{<Typewriter onInit={animation} />}</h1>
       </div>
     </div>
   );
